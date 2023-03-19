eb8532ab12a9a93835a1818fc6104fd3bd225c7f
tracer-ics2017 <tracer@njuics.org>
2023-03-19 14:52:37 +0000

    >  compile 2013631 pyt Linux 180b072ba2fb 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  14:52:37 up 45 min,  1 user,  load average: 0.00, 0.02, 0.00 1953b975ad1aadc97646cb7be97d432b1d64ccdb

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -19,5 +19,87 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp(){
+	if(free_ ==NULL){
+		printf("No free space.\n");
+		assert(0);
+	}
+	if(head == NULL){
+		head = free_;
+		free_ = free_ -> next;
+		head->next = NULL;
+		head->used = true;
+		return head;
+	}
+	else{
+		WP* current = head;
+		while(current->next != NULL){
+			current = current->next;
+		}
+		current->next = free_;
+		free_ = free_->next;
+		current->next->next = NULL;
+		current->next->used = true;
+		return current->next;
+	}
+}
 
+void free_wp(int no){
+  WP* curr_head = head;
+  WP* curr_free = free_;
+  if (0 > no || no >= 32) {
+    printf("Error: %d is not an acceptable watchpoint.\n", no);
+  }
+  if (curr_head == NULL || (curr_head->next == NULL && curr_head->NO != no)) {
+    printf("Error: try to free a nonexistent watchpoint.\n");
+    assert(0);
+  }
+  if (curr_head->NO == no) {
+    printf("%d\n", no);
+    while(curr_free->next != NULL) {
+      curr_free = curr_free->next;
+    }
+    curr_free->next = curr_head;
+    if (curr_head->next != NULL) {
+      head = curr_head->next;
+    }
+    else {
+      head = NULL;
+    }
+    curr_free->next->used = false;
+    curr_free->next->expr = NULL;
+    curr_free->next->next = NULL;
+    
+    return;
+  }
+  if (curr_free == NULL) {
+    while(curr_head != NULL && curr_head->next->NO != no) {
+      curr_head = curr_head->next;
+    }
+    if (curr_head == NULL) {
+      printf("Error: watchpoint %d is not found to be used.\n", no);
+    }
+    curr_head->next->used = false;
+    curr_head->next->expr = NULL;
+    curr_free = curr_head->next;
+    curr_head->next = curr_head->next->next;
+    curr_free->next = NULL;
+  }
+  else {
+    while(curr_head != NULL && curr_head->next->NO != no) {
+      curr_head = curr_head->next;
+    }
+    while(curr_free->next != NULL) {
+      curr_free = curr_free->next;
+    }
+    if (curr_head == NULL) {
+      printf("Error: watchpoint %d is not found to be used.\n", no);
+    }
+    curr_head->next->used = false;
+    curr_head->next->expr = NULL;
+    curr_free->next = curr_head->next;
+    curr_head->next = curr_head->next->next;
+    curr_free->next->next = NULL;
+  }
 
+}
