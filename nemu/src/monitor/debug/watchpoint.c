79e78f04ae12de590e6106c6a1873b06312a1170
tracer-ics2017 <tracer@njuics.org>
2023-03-21 00:54:14 +0000

    >  compile 2013631 pyt Linux 180b072ba2fb 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  00:54:14 up 20 min,  1 user,  load average: 0.05, 0.02, 0.00 7f04bebfb4738073dd99635952a826d182bf770

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -10,9 +10,10 @@ void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
-    wp_pool[i].next = &wp_pool[i + 1];
+	wp_pool[i].used = false;
+	if(i == NR_WP - 1) wp_pool[i].next = NULL;
+	else wp_pool[i].next = &wp_pool[i + 1];
   }
-  wp_pool[NR_WP - 1].next = NULL;
 
   head = NULL;
   free_ = wp_pool;
