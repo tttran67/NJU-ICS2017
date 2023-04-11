68bf415133d6c3ee17c82f04068d1096f859f090
tracer-ics2017 <tracer@njuics.org>
2023-04-11 08:46:17 +0000

    >  compile 2013631 pyt Linux 180b072ba2fb 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  08:46:17 up  3:04,  1 user,  load average: 0.05, 0.04, 0.00 8ad966bcab2960dbc3f0ebb49845f614f3029243

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -6,3 +6,29 @@ make_EHelper(operand_size);
 
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+
+// control.c
+make_EHelper(jmp);
+make_EHelper(call);
+make_EHelper(ret);
+
+// data-mov.c
+make_EHelper(push);
+make_EHelper(pop);
+
+// arith.c
+make_EHelper(add);
+make_EHelper(adc);
+make_EHelper(cmp);
+make_EHelper(sub);
+make_EHelper(sbb);
+make_EHelper(mul);
+make_EHelper(sub);
+
+// logic.c
+make_EHelper(test);
+make_EHelper(and);
+make_EHelper(or);
+make_EHelper(xor);
+
+
