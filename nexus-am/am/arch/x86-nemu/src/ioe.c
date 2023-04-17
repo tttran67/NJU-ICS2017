efb3bf3c0ba604fb0810d4fa94d8fc9bb245d59a
tracer-ics2017 <tracer@njuics.org>
2023-04-17 01:25:44 +0000

    >  compile 2013631 pyt Linux 180b072ba2fb 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  01:25:44 up 31 min,  1 user,  load average: 0.08, 0.17, 0.14 5f7a3d49109af6261b1f20c5574760fff075d88e

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -9,7 +9,9 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-  return 0;
+//  return 0;
+  unsigned long ms = inl(RTC_PORT) -  boot_time;
+  return ms;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
