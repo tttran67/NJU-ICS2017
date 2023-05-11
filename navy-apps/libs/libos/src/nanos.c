699890ddb5c5d5c5a138cf3e5643a3c1146aba46
tracer-ics2017 <tracer@njuics.org>
2023-05-11 22:14:04 +0800

    >  run 2013631 pyt Linux pyt-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:14:04 up 3 min,  1 user,  load average: 0.32, 0.37, 0.17 f297052fdd874f9026d4905b627ce01768b58d43

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -36,7 +36,7 @@ void *_sbrk(intptr_t increment){
   intptr_t old_brk = brk;
   intptr_t new_brk = old_brk + increment;
   if (_syscall_(SYS_brk, (uintptr_t)new_brk, 0, 0) == 0) {
-    brk = new_brk;
+    brk += increment;//【pyt】
     return (void *)old_brk;
   }
   return (void *)-1;
