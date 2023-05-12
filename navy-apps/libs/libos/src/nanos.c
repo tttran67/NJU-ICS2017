15e9455b316148fafe22f6f9a7a22c74dcecbea8
tracer-ics2017 <tracer@njuics.org>
2023-05-12 13:07:18 +0800

    >  compile 2013631 pyt Linux pyt-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  13:07:18 up 18 min,  1 user,  load average: 0.59, 0.28, 0.15 af74e3193c3b3e4ed2bfbb9d6d1792b7c87f0ae9

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -23,7 +23,8 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
+  //_exit(SYS_open);
+  _syscall_(SYS_open, (uintptr_t)path, (uintptr_t)flags, (uintptr_t)mode);
 }
 
 int _write(int fd, void *buf, size_t count){
@@ -43,15 +44,18 @@ void *_sbrk(intptr_t increment){
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _exit(SYS_read);
+  //_exit(SYS_read);
+  _syscall_(SYS_read, (uintptr_t)fd, (uintptr_t)buf, (uintptr_t)count);
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
+  //_exit(SYS_close);
+  _syscall_(SYS_close, (uintptr_t)fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
+  //_exit(SYS_lseek);
+  _syscall_(SYS_lseek, (uintptr_t)fd, (uintptr_t)offset, (uintptr_t)whence);
 }
 
 // The code below is not used by Nanos-lite.
@@ -64,7 +68,7 @@ int _fstat(int fd, struct stat *buf) {
 
 int execve(const char *fname, char * const argv[], char *const envp[]) {
   assert(0);
-  return -1;
+  return -1;//【peer】
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
